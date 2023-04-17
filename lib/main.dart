c079dbb85d52bea6f924e5f5a8a95ab2c34089e3
umairmalik007 <115790558+umairmalik007@users.noreply.github.com>
2023-04-17 20:03:17 +0500

    chnaged app name

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       builder: (BuildContext context, child) => GetMaterialApp(
+        title: 'Bank App',
         theme: ThemeData(
             elevatedButtonTheme: ElevatedButtonThemeData(
                 style: ElevatedButton.styleFrom(elevation: 0)),
